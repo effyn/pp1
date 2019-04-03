43d31300519e72031fbed2b8fb100c486dcc98d5
Evan Wheeler <effyn@gmx.com>
2019-04-03 17:36:29 -0400

    Add largest function and test it

--- functions.php
+++ functions.php
@@ -6,3 +6,22 @@ function printArr($arr)
         echo "<p>$item</p>";
     }
 }
+
+function largest($arr)
+{
+    if (empty($arr))
+    {
+        return null;
+    }
+
+    $max = $arr[0];
+    for ($i = 1; $i < sizeof($arr); $i++)
+    {
+        $item = $arr[$i];
+        if ($item > $max)
+        {
+            $max = $item;
+        }
+    }
+    return $max;
+}
--- index.php
+++ index.php
@@ -11,10 +11,14 @@
 <h1>Pair Program 1</h1>
 
 <?php
-    include("functions.php");
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    include('functions.php');
 
     $numbers = array(7,9,8,9,8,8,6);
     printArr($numbers);
+    echo '<p>Largest: ' . largest($numbers) . '</p>';
 ?>
 
 </body>
